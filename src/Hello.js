59d8d7e0dfd80be5fe28cf91a8b57df7ea4d3c75
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-10-13 11:29:19 -0400

    added props to Hello Component

--- src/Hello.js
+++ src/Hello.js
@@ -4,9 +4,9 @@ import './Hello.css';
 class Hello extends Component {
     render() {
         return (
-          <div class='f1 tc'>
+          <div className='f1 tc'>
             <h1>Hello World</h1>
-            <p>Welcome to React</p>
+            <p>{this.props.greeting}</p>
           </div>
         ); 
     }
