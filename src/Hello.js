e2553c97c364ec884e242c8d6b74bc25d9bb2bc5
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-10-13 11:16:49 -0400

    added Hello Component

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+    render() {
+        return (
+          <div class='f1 tc'>
+            <h1>Hello World</h1>
+            <p>Welcome to React</p>
+          </div>
+        ); 
+    }
+}
+
+export default Hello;
