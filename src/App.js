37963a34bb00355f3f6190de3eed040a9e6e1580
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-10-18 09:04:37 -0400

    use json API to fetch a response

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
-import { robots } from "./robots";
 
 
 
@@ -10,9 +9,15 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-          robots: robots,
+          robots: [],
           searchfield: ""
         };
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(users => this.setState({ robots: users}));
     }
 
     onSearchChange = (e) => {
@@ -24,14 +29,18 @@ class App extends Component {
           return robot.name
             .toLowerCase()
             .includes(this.state.searchfield.toLowerCase());
-        });
-        return (
-        <div className='tc'>
-            <h1 className='f1'>RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots={filteredRobots}/>
-        </div>
-    );
+        })
+        if (this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        } else {
+            return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots}/>
+            </div>    
+            );
+        }    
     }
 }
 
