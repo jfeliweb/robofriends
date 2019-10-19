4419ef8217257ecf9527255cb9f0db61f52912ab
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-10-18 20:11:06 -0400

    added error handling via Error Boundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 
@@ -32,14 +33,16 @@ class App extends Component {
             .toLowerCase()
             .includes(searchfield.toLowerCase());
         })
-        return !robots.length ?
-          <h1>Loading</h1> :
-        (
+        return !robots.length ? (
+          <h1>Loading</h1>
+        ) : (
           <div className="tc">
             <h1 className="f1">RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange} />
             <Scroll>
-              <CardList robots={filteredRobots} />
+              <ErrorBoundry>
+                <CardList robots={filteredRobots} />
+              </ErrorBoundry>
             </Scroll>
           </div>
         );
