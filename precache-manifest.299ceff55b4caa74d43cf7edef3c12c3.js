c7cb2758935239fa899a9867b179fde38dae0c61
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2019-10-19 21:06:27 -0400

    Updates

---
+++ precache-manifest.299ceff55b4caa74d43cf7edef3c12c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80840e7bad48bd4f5214e1b0d7325281",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4755e24a969faca36da7",
+    "url": "/robofriends/static/css/main.4d9c516b.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "4755e24a969faca36da7",
+    "url": "/robofriends/static/js/main.ffadd28a.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
